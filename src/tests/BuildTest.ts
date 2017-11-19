24ecd7a868aba228e161d8f3b1148d1d0ac9bfd0
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2017-11-19 16:42:50 +0100

    Remove useless test

--- src/tests/BuildTest.ts
+++ src/tests/BuildTest.ts
@@ -1,25 +1,14 @@
 import * as chai from 'chai';
-import {spawnSync} from "child_process";
+import {spawn, spawnSync} from "child_process";
 
 const assert = chai.assert;
 
 describe('BuildTest', () => {
 
-    it('Build task should execute without errors', (done) => {
+    it.skip('Package task should execute without errors', (done) => {
         assert.doesNotThrow(() => {
             try {
-                spawnSync('npm run build');
-                done();
-            } catch (e) {
-                done(e);
-            }
-        });
-    });
-
-    it('Package task should execute without errors', (done) => {
-        assert.doesNotThrow(() => {
-            try {
-                spawnSync('npm run build');
+                spawn('npm run package');
                 done();
             } catch (e) {
                 done(e);
