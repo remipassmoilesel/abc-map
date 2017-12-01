0fc453e729a1d258a185898b9dcb7374208cc957
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2017-12-01 22:12:05 +0100

    Add draw tools

--- src/gui/components/map/GeoMapComponent.ts
+++ src/gui/components/map/GeoMapComponent.ts
@@ -4,8 +4,12 @@ import * as L from 'leaflet';
 import * as _ from 'lodash';
 import {MainStore} from "../../lib/store/store";
 import {AbstractMapLayer} from "../../../api/entities/layers/AbstractMapLayer";
-import './style.scss'
 import {LeafletLayerFactory} from "../../lib/LeafletLayerFactory";
+// Import style
+import './style.scss'
+// Import plugins
+import 'leaflet-draw/dist/leaflet.draw.js';
+import {FeatureGroup} from "leaflet";
 
 let mapIdCounter = 0;
 const layerFactory = new LeafletLayerFactory();
@@ -20,10 +24,12 @@ export default class GeoMapComponent extends Vue {
     public mapId = `map-${mapIdCounter++}`;
     public height: number = 500;
     private map: L.Map;
+    private editableLayers: FeatureGroup<any>;
 
     public mounted() {
         this.height = this.getMaximumHeight();
         this.map = L.map(this.mapId).setView([45.18, 5.72], 13);
+        this.setupDraw();
     }
 
     public updated() {
@@ -38,6 +44,70 @@ export default class GeoMapComponent extends Vue {
         _.forEach(layers, (layer) => {
             this.map.addLayer(layerFactory.getLeafletLayer(layer));
         });
+
+    }
+
+    private setupDraw(){
+        this.editableLayers = new L.FeatureGroup();
+        this.map.addLayer(this.editableLayers);
+
+        // var MyCustomMarker = L.Icon.extend({
+        //     options: {
+        //         shadowUrl: null,
+        //         iconAnchor: new L.Point(12, 12),
+        //         iconSize: new L.Point(24, 24),
+        //         iconUrl: 'link/to/image.png'
+        //     }
+        // });
+
+        var options = {
+            position: 'topright',
+            draw: {
+                polyline: {
+                    shapeOptions: {
+                        color: '#f357a1',
+                        weight: 10
+                    }
+                },
+                polygon: {
+                    allowIntersection: false, // Restricts shapes to simple polygons
+                    drawError: {
+                        color: '#e1e100', // Color the shape will turn when intersects
+                        message: '<strong>Oh snap!<strong> you can\'t draw that!' // Message that will show when intersect
+                    },
+                    shapeOptions: {
+                        color: '#bada55'
+                    }
+                },
+                circle: true, // Turns off this drawing tool
+                rectangle: {
+                    shapeOptions: {
+                        clickable: false
+                    }
+                },
+                // marker: {
+                //     icon: new MyCustomMarker()
+                // }
+            },
+            edit: {
+                featureGroup: this.editableLayers, //REQUIRED!!
+                remove: false
+            }
+        };
+
+        const drawControl = new (L.Control as any).Draw(options);
+        this.map.addControl(drawControl);
+
+       // this. map.on(L.Draw.Event.CREATED, function (e) {
+       //      var type = e.layerType,
+       //          layer = e.layer;
+       //
+       //      if (type === 'marker') {
+       //          layer.bindPopup('A popup!');
+       //      }
+       //
+       //      this.editableLayers.addLayer(layer);
+       //  });
 
     }
 
