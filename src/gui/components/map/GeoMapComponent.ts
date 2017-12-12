19817e6c1b9ba800a6ea1991db6cf3f90de5bf12
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2017-12-12 23:12:01 +0100

    Fix map size

--- src/gui/components/map/GeoMapComponent.ts
+++ src/gui/components/map/GeoMapComponent.ts
@@ -147,7 +147,7 @@ export default class GeoMapComponent extends Vue {
     }
 
     public getMaximumHeight(): number {
-        return window.innerWidth / 100 * 60;
+        return window.innerWidth / 100 * 68;
     }
 
     public onDragOver(event) {
