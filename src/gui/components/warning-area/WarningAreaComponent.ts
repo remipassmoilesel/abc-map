8115890af443d504cd2686dea2b8e04b2bddb126
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2017-12-29 18:02:01 +0100

    Improve connectivity test

--- src/gui/components/warning-area/WarningAreaComponent.ts
+++ src/gui/components/warning-area/WarningAreaComponent.ts
@@ -5,7 +5,11 @@ import {UiShortcuts} from '../../lib/UiShortcuts';
 import {AbstractUiComponent} from '../AbstractUiComponent';
 import {MainStore} from '../../lib/store/store';
 import {StoreWrapper} from '../../lib/store/StoreWrapper';
+import {Logger, LogLevel} from '../../../api/dev/Logger';
 import './style.scss';
+
+const logger = Logger.getLogger('WarningAreaComponent');
+logger.setLevel(LogLevel.ERROR);
 
 @Component({
     template: require('./template.html'),
@@ -22,16 +26,28 @@ export class WarningAreaComponent extends AbstractUiComponent {
     public storeWrapper: StoreWrapper;
 
     public warningMessage: string = '';
+    private connectionTestInterval: number;
 
     constructor() {
         super();
     }
 
     public mounted() {
+        this.testConnectivity();
+        this.connectionTestInterval = (setInterval(this.testConnectivity.bind(this), 4000) as any);
+    }
+
+    public testConnectivity() {
+        logger.debug('Testing connectivity');
         $.get('http://abc-map.fr')
             .catch((error) => {
                 this.warningMessage = 'You are not connected to Internet, many features will be unavailable.';
+                logger.debug('Error while connecting to internet: ', error);
             });
+    }
+
+    public beforeDestroy() {
+        clearInterval(this.connectionTestInterval);
     }
 
 }
