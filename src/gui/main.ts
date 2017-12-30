1b11e88fbda5760a5b0558781cd577288662c230
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2017-12-30 10:59:08 +0100

    Improve error handling

--- src/gui/main.ts
+++ src/gui/main.ts
@@ -2,6 +2,10 @@ import {Toaster} from './lib/Toaster';
 import Vue from 'vue';
 import {ElectronUtilities} from '../api/dev/ElectronDevUtilities';
 import {Logger} from '../api/dev/Logger';
+
+import * as sourceMapSupport from 'source-map-support';
+sourceMapSupport.install();
+
 // create vuex store
 import {store} from './lib/store/store';
 // import router
@@ -18,13 +22,14 @@ import './lib/mixins';
 // Import tests
 import './tests/tests.ts';
 
-require('source-map-support').install();
-
 const logger = Logger.getLogger('gui/main.ts');
 logger.info('Starting main app');
 
 Vue.config.errorHandler = (err, vm, info) => {
-    console.error({err, vm, info});
+    console.error('Vuejs error handler. Error, Vm, info: ');
+    console.error(err);
+    console.error(vm);
+    console.error(info);
     Toaster.error(JSON.stringify(info));
 };
 
