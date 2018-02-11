3e38239786bcd360ceedf5d53ab592157dfa04bb
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-02-11 15:29:20 +0100

    Fix geocoder

--- src/api/geocoder/NominatimGeocoder.ts
+++ src/api/geocoder/NominatimGeocoder.ts
@@ -23,22 +23,30 @@ export class NominatimGeocoder extends AbstractGeocoder {
     private resultsLimit = 10;
     private baseUrl = 'http://nominatim.openstreetmap.org/search?format=json&limit=:limit&q=:query';
 
-    public geocode(query: string): Promise<GeocodingResult[]> {
-        return this.requestRaw(query).then((data: INominatimResult[]) => {
-            const res: GeocodingResult[] = _.map(data, (res: INominatimResult) => {
-                return new GeocodingResult(
-                    res.display_name,
-                    Number(res.lat),
-                    Number(res.lon),
-                );
-            });
-            return res;
+    public async geocode(query: string): Promise<GeocodingResult[]> {
+        const data: INominatimResult[] = await this.requestRaw(query);
+        const res: GeocodingResult[] = _.map(data, (res: INominatimResult) => {
+            return new GeocodingResult(
+                res.display_name,
+                Number(res.lat),
+                Number(res.lon),
+            );
         });
+        return res;
     }
 
     public requestRaw(query: string): Promise<INominatimResult[]> {
         const requestUrl = this.getRequestUrl(query);
-        return (request({uri: requestUrl, json: true}) as any);
+
+        // FIXME: remove dev user agent
+
+        /* tslint:disable:max-line-length */
+        return request({
+            uri: requestUrl, json: true, headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36',
+            },
+        }) as any;
+        /* tslint:enable:max-line-length */
     }
 
     private getRequestUrl(query: string): string {
