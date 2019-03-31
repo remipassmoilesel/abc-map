4ee5df36c8513af9216c9bbe94d2da9b5321f0fe
Rémi Passmoilesel <r.passmoilesel@protonmail.com>
2019-03-31 23:23:07 +0200

    Improve electorn window

--- api/src/electron-main.ts
+++ api/src/electron-main.ts
@@ -1,37 +1,26 @@
 import { app, BrowserWindow } from "electron";
 
-// Gardez une reference globale de l'objet window, si vous ne le faites pas, la fenetre sera
-// fermee automatiquement quand l'objet JavaScript sera garbage collected.
 let win: BrowserWindow | null;
 
 function createWindow() {
-    // Créer le browser window.
-    win = new BrowserWindow({ width: 800, height: 600 });
-
-    // et charge le index.html de l'application.
+    win = new BrowserWindow({ width: 800, height: 600, show: false });
+    win.setMenu(null);
+    win.setMenuBarVisibility(false);
+    win.maximize();
     win.loadURL("http://localhost:8080");
 
-    // Ouvre les DevTools.
-    win.webContents.openDevTools();
+    win.once('ready-to-show', () => {
+        win && win.show()
+    });
 
-    // Émit lorsque la fenêtre est fermée.
     win.on("closed", () => {
-        // Dé-référence l'objet window , normalement, vous stockeriez les fenêtres
-        // dans un tableau si votre application supporte le multi-fenêtre. C'est le moment
-        // où vous devez supprimer l'élément correspondant.
         win = null;
     });
 }
 
-// Cette méthode sera appelée quant Electron aura fini
-// de s'initialiser et sera prêt à créer des fenêtres de navigation.
-// Certaines APIs peuvent être utilisées uniquement quand cet événement est émit.
 app.on("ready", createWindow);
 
-// Quitte l'application quand toutes les fenêtres sont fermées.
 app.on("window-all-closed", () => {
-    // Sur macOS, il est commun pour une application et leur barre de menu
-    // de rester active tant que l'utilisateur ne quitte pas explicitement avec Cmd + Q
     if (process.platform !== "darwin") {
         app.quit();
     }
