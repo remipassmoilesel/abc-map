5ed542edaf6346799ae5934a369f508896caf3e6
Rémi Passmoilesel <r.passmoilesel@protonmail.com>
2019-04-02 00:40:46 +0200

    Fix build script

--- build.sh
+++ build.sh
@@ -5,9 +5,7 @@ set -e
 
 # TODO: add tests
 
-cd shared && npm install && npm run lint && npm run build
-
-cd ../api && npm install && npm run lint && npm run build
-
-cd ../gui && npm install && npm run lint && npm run build
+cd shared && npm install && npm run lint && npm run build && \
+    cd ../api && npm install && npm run lint && npm run build && \
+    cd ../gui && npm install && npm run lint && npm run build
 
