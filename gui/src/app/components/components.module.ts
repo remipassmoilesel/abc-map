dd963cb6c10476c0435589fcf8a0c16bcabd2412
Rémi Passmoilesel <r.passmoilesel@protonmail.com>
2019-04-06 15:40:00 +0200

    Add side menu, refactor main app structure

--- gui/src/app/components/components.module.ts
+++ gui/src/app/components/components.module.ts
@@ -3,16 +3,18 @@ import {CommonModule} from '@angular/common';
 import {TopBarComponent} from './top-bar/top-bar.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {RouterModule} from '@angular/router';
+import {RightMenuComponent} from './right-menu/right-menu.component';
 
 @NgModule({
-  declarations: [TopBarComponent],
-  exports: [
-    TopBarComponent
-  ],
+  declarations: [TopBarComponent, RightMenuComponent],
   imports: [
     CommonModule,
     NgbModule,
     RouterModule
+  ],
+  exports: [
+    TopBarComponent,
+    RightMenuComponent
   ]
 })
 export class ComponentsModule {
