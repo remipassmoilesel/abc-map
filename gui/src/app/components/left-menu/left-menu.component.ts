9a02af1dc788707acbd4ef0be5f6febbe8e45a3b
Rémi Passmoilesel <r.passmoilesel@protonmail.com>
2019-04-12 23:56:26 +0200

    Add left bar

---
+++ gui/src/app/components/left-menu/left-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'abc-left-menu',
+  templateUrl: './left-menu.component.html',
+  styleUrls: ['./left-menu.component.scss']
+})
+export class LeftMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
