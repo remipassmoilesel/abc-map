93deff8d9a1bc8ba5d135e5ff63bbc67b8a7f6cb
Rémi Passmoilesel <r.passmoilesel@protonmail.com>
2019-05-02 22:41:28 +0200

    Improve display, improve register form

--- gui/src/app/lib/authentication/authentication.service.ts
+++ gui/src/app/lib/authentication/authentication.service.ts
@@ -1,15 +1,23 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {AuthenticationClient} from './AuthenticationClient';
 import {IUserCreationRequest} from 'abcmap-shared';
+import {ToastService} from '../notifications/toast.service';
+import {tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor(private client: AuthenticationClient) { }
+  constructor(private client: AuthenticationClient,
+              private toasts: ToastService) {
+  }
 
-  public registerUser(request: IUserCreationRequest){
-    return this.client.registerUser(request);
+  public registerUser(request: IUserCreationRequest) {
+    return this.client.registerUser(request)
+      .pipe(
+        tap(res => this.toasts.info('Vous êtes inscrit !'),
+          err => this.toasts.error('Erreur lors de l\'inscription, veuillez réessayer plus tard !'))
+      );
   }
 }
--- gui/src/app/lib/map/DrawingTool.ts
+++ gui/src/app/lib/map/DrawingTool.ts
@@ -11,8 +11,8 @@ export class DrawingTools {
 
   // TODO: find better icons
   public static readonly Point = new DrawingTool('Point', 'fa-circle', 'Point');
-  public static readonly LineString = new DrawingTool('LineString', 'fa-chart-line', 'Ligne');
-  public static readonly Polygon = new DrawingTool('Polygon', 'fa-map-marker', 'Polygone');
+  public static readonly LineString = new DrawingTool('LineString', 'fa-slash', 'Ligne');
+  public static readonly Polygon = new DrawingTool('Polygon', 'fa-draw-polygon', 'Polygone');
   public static readonly None = new DrawingTool('None', 'fa-times', 'Aucun');
 
   public static readonly All = [
