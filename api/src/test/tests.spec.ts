5ab11732aa2379bc3cd22f255c2641be11fedcf7
Rémi Passmoilesel <r.passmoilesel@protonmail.com>
2019-05-03 22:50:12 +0200

    Add first test setup

---
+++ api/src/test/tests.spec.ts
@@ -0,0 +1,10 @@
+import {assert} from 'chai';
+
+
+describe('Array', () => {
+    describe('#indexOf()', () => {
+        it('should return -1 when the value is not present', () => {
+            assert.equal([1, 2, 3].indexOf(4), -1);
+        });
+    });
+});
