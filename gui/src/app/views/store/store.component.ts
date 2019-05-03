69454be71afdfbbd22d908ff2586c33eed392a53
Rémi Passmoilesel <r.passmoilesel@protonmail.com>
2019-05-04 00:47:47 +0200

    Improve store

--- gui/src/app/views/store/store.component.ts
+++ gui/src/app/views/store/store.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {AuthenticationService} from '../../lib/authentication/authentication.service';
 
 @Component({
   selector: 'abc-store',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StoreComponent implements OnInit {
 
-  constructor() { }
+  searchForm?: FormGroup;
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
   }
 
+  ngOnInit() {
+    this.initForm();
+  }
+
+  private initForm() {
+    this.searchForm = this.formBuilder.group({
+      query: [''],
+    });
+  }
+
+  register() {
+    if (!this.searchForm) {
+      return;
+    }
+
+    // TODO
+  }
+
+  search() {
+    // TODO
+  }
 }
