7eaa1ec258af51ded1423034081c8804ec49cde4
Rémi Passmoilesel <r.passmoilesel@protonmail.com>
2019-06-03 00:03:45 +0200

    Upload documents on startup

--- api/src/data/DatastoreInit.ts
+++ api/src/data/DatastoreInit.ts
@@ -1,12 +1,19 @@
 import {DatastoreService} from './DatastoreService';
 import * as fs from 'fs';
 import {IApiConfig} from '../IApiConfig';
+import {Logger} from 'loglevel';
+import glob = require('glob');
+import loglevel = require('loglevel');
+import * as path from "path";
 
 export class DatastoreInit {
 
-    constructor(private datastore: DatastoreService,
-                private apiConfig: IApiConfig) {
+    protected logger: Logger = loglevel.getLogger('DatastoreInit');
+    private basePath: string;
 
+    constructor(private datastore: DatastoreService,
+                apiConfig: IApiConfig) {
+        this.basePath = apiConfig.publicDir.rootPath;
     }
 
     public async init(): Promise<void> {
@@ -14,8 +21,25 @@ export class DatastoreInit {
     }
 
     private async importFromPublicDir(): Promise<void> {
-        fs.readdirSync(this.apiConfig.publicDir.rootPath).forEach(file => {
-            console.log(file);
+        const files = glob.sync('**', {
+            cwd: this.basePath,
+        });
+
+        files.forEach(filePath => {
+            const fullPath = path.resolve(this.basePath, filePath);
+            if (fs.statSync(fullPath).isDirectory()) {
+                return true;
+            }
+
+            const content = fs.readFileSync(fullPath);
+            const docPath = 'public/' + filePath;
+
+            this.logger.info(`Importing: ${docPath}`);
+
+            this.datastore.storeDocument('public', docPath, content)
+                .catch(err => this.logger.error(`Error while storing file. document=${docPath}`, err));
+            this.datastore.cacheDocumentAsGeojson(docPath, content)
+                .catch(err => this.logger.error(`Error while caching file. document=${docPath}`, err));
         });
     }
 }
