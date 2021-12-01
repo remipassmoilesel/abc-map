ca5127b359aeed579f83f0bd4a21a0ecc7fcc5f9
Rémi Passmoilesel <r.passmoilesel@protonmail.com>
2021-12-01 21:20:38 +0000

    Improve device warning

--- packages/frontend/src/components/device-warning-modal/DeviceWarningModal.tsx
+++ packages/frontend/src/components/device-warning-modal/DeviceWarningModal.tsx
@@ -16,75 +16,51 @@
  * Public License along with Abc-Map. If not, see <https://www.gnu.org/licenses/>.
  */
 
-import React, { Component, ReactNode } from 'react';
-import { Modal } from 'react-bootstrap';
+import React, { useCallback, useEffect, useState } from 'react';
 import * as Bowser from 'bowser';
-import { Logger } from '@abc-map/shared';
-import phoneWarning from './phone-warning.png';
+import mainLogo from '../../assets/main-icon.png';
 import { prefixedTranslation } from '../../i18n/i18n';
 import { withTranslation } from 'react-i18next';
 import Cls from './DeviceWarningModal.module.scss';
 
-const logger = Logger.get('DeviceWarningModal.tsx');
-
-interface State {
-  visible: boolean;
-}
-
 const t = prefixedTranslation('DeviceWarningModal:');
 
-class DeviceWarningModal extends Component<{}, State> {
-  constructor(props: {}) {
-    super(props);
-    this.state = { visible: false };
-  }
+function DeviceWarningModal() {
+  const [visible, setVisible] = useState(false);
 
-  public render(): ReactNode {
-    const visible = this.state.visible;
-    if (!visible) {
-      return <div />;
-    }
-
-    return (
-      <Modal show={visible} onHide={this.handleClose} backdrop={'static'} dialogClassName={Cls.deviceWarning}>
-        <Modal.Header closeButton>
-          <Modal.Title>{t('Warning')}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <div className={'d-flex align-items-center'}>
-            <img src={phoneWarning} width={'200'} className={'m-3'} alt={'Ordinateur de bureau conseillé'} />
-            <div className={'m-3'}>
-              <div data-cy={'device-warning'}>{t('AbcMap_may_not_work_properly')} 🤔</div>
-              <div className={'my-3'} dangerouslySetInnerHTML={{ __html: t('AbcMap_is_designed_for_desktop_computers') }} />
-            </div>
-          </div>
-          <div className={'d-flex justify-content-end'}>
-            <button className={'btn btn-primary'} onClick={this.handleClose} data-cy="device-warning-confirm">
-              {t('I_understand')}
-            </button>
-          </div>
-        </Modal.Body>
-      </Modal>
-    );
-  }
-
-  public componentDidMount() {
-    if (!this.isOptimalDevice()) {
-      this.setState({ visible: true });
-    }
-  }
-
-  public handleClose = () => {
-    this.setState({ visible: false });
-  };
-
-  public isOptimalDevice(): boolean {
+  const isOptimalDevice = useCallback(() => {
     const browser = Bowser.getParser(window.navigator.userAgent);
     const deviceSupported = browser.getPlatform().type === 'desktop';
     const browserSupported = ['chrome', 'electron', 'firefox'].indexOf(browser.getBrowserName(true)) !== -1;
     const screenSizeSupported = window.innerWidth >= 1366 && window.innerHeight >= 768;
     return deviceSupported && browserSupported && screenSizeSupported;
+  }, []);
+
+  useEffect(() => {
+    if (!isOptimalDevice()) {
+      setVisible(true);
+    }
+  }, [isOptimalDevice]);
+
+  if (!visible) {
+    return <div />;
   }
+
+  return (
+    <div className={Cls.modal}>
+      <img src={mainLogo} alt={'Abc-Map'} />
+
+      <div className={Cls.title}>{t('Hello')} 👋</div>
+      <div className={Cls.explanation1} data-cy={'device-warning'} dangerouslySetInnerHTML={{ __html: t('AbcMap_is_designed_for_desktop_computers') }} />
+      <div className={Cls.explanation2}>{t('You_may_come_across_some_bugs')} 🐛</div>
+
+      <div className={'d-flex justify-content-end m-5'}>
+        <button onClick={() => setVisible(false)} className={'btn btn-primary'} data-cy="device-warning-confirm">
+          {t('I_understand')}
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default withTranslation()(DeviceWarningModal);
